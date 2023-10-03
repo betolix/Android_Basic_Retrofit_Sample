dc61d99257ecdaf4bfbcd51f450ce81cc7acfd38
beto <betolix@gmail.com>
2023-10-03 10:08:53 -0400

    Android Basic Retrofit Sample

---
+++ app/src/main/java/io/h3llo/retrofitsample/data/RetrofitService.kt
@@ -0,0 +1,26 @@
+package io.h3llo.retrofitsample.data
+
+import io.h3llo.retrofitsample.model.RemoteResult
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RetrofitService {
+
+    @GET("discover/movie?sort_by=popularity.desc")
+    suspend fun listPopularMovies(
+        @Query("api_key") apiKey: String,
+        @Query("region") region: String
+    ): RemoteResult
+
+    object RetrofitServiceFactory {
+        fun makeRetrofitService(): RetrofitService {
+            return Retrofit.Builder()
+                    .baseUrl("https://api.themoviedb.org/3/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build().create(RetrofitService::class.java)
+        }
+    }
+
+}
